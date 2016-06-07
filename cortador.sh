1764ea844ec44548b46661cbe4258994407ebb0d
NeoRanger <neoranger@openmailbox.com>
2016-06-07 02:44:11 -0300

    new file:   ArchivoLog 	modified:   cortador.sh

--- cortador.sh
+++ cortador.sh
@@ -4,7 +4,7 @@
 echo "Introduzca la fecha a buscar (formato AAAA/MM/DD)";read FECHA
 
 #Busco la linea pedida en el archivo y me guardo la posicion
-NRO_LINEA=$(cat ArchivoLog | grep -n "$FECHA" | cut -d':' -f1)
+NRO_LINEA=$(head -1 | cat ArchivoLog | grep -n "$FECHA" | cut -d':' -f1 | tail -1)
 #Cuento el total de lineas del archivo
 TOTAL_LINEAS=$(wc -l ArchivoLog | cut -d' ' -f1)
 #Hago la diferencia del total de lineas con la linea pedida
@@ -14,5 +14,3 @@ ANIO=$(echo $FECHA | cut -d'/' -f1)
 MES=$(echo $FECHA | cut -d'/' -f2)
 DIA=$(echo $FECHA | cut -d'/' -f3)
 tail -n $DIFERENCIA ArchivoLog > ArchivoLog_"$ANIO"_"$MES"_"$DIA"
-
-echo ---------Fin del script-------------
