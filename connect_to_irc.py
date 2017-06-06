41eab86a05ea7c636a305cda4e4979a99a59c8d7
NeoRanger <neoranger@openmailbox.org>
2017-06-06 20:53:22 -0300

    new file:   connect_to_irc.py

---
+++ connect_to_irc.py
@@ -0,0 +1,29 @@
+import sys
+import socket
+import string
+
+HOST="irc.freenode.net"
+PORT=6667
+NICK="NeoRanger"
+IDENT="sdada"
+REALNAME="sdada"
+readbuffer=""
+
+s=socket.socket( )
+s.connect((HOST, PORT))
+s.send("NICK %s\r\n" % NICK)
+s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+
+while 1:
+    readbuffer=readbuffer+s.recv(1024)
+    temp=string.split(readbuffer, "\n")
+    readbuffer=temp.pop( )
+
+    for line in temp:
+        line=string.rstrip(line)
+        line=string.split(line)
+
+        if(line[0]=="PING"):
+            s.send("PONG %s\r\n" % line[1])
+
+Pasted a minute ago — Expires in 1 day
