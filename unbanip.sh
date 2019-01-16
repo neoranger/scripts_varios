a5e97740093e868821056f860362060c08df260e
neoranger <neoranger@disroot.org>
2019-01-16 15:49:12 -0300

    modified:   unbanip.sh

--- unbanip.sh
+++ unbanip.sh
@@ -6,7 +6,7 @@
 
 echo "Conoce la IP a desbloquear? Y/N"
 read respuesta1 
-if [ -z $respuesta1 ]; then
+if [ -z "$respuesta1" ]; then
     echo "Respuesta erronea o vacía. Se aborta el programa"
     exit 0
 fi
@@ -14,7 +14,7 @@ fi
 if [ "$respuesta1" == "Y" ] || [ "$respuesta1" == "y" ]; then
     echo "Ingresar IP a desbanear: "
     read ip
-    if [ -z $respuesta1 ]; then
+    if [ -z "$respuesta1" ]; then
         echo "Respuesta erronea o vacía. Se aborta el programa"
     exit 0
 fi
@@ -31,7 +31,7 @@ echo "----------------------------------------------------------"
 echo "Conoce la celda? Y/N"
 read respuesta2
 
-if [ -z $respuesta2 ]; then
+if [ -z "$respuesta2" ]; then
     echo "Respuesta erronea o vacía. Se aborta el programa"
     exit 0
 fi
@@ -51,7 +51,7 @@ echo "----------------------------------------------------------"
 echo "La jail a usar es: $jail y la ip a desbanear es: $ip. Es correcto? Y/N"
 read respuesta3
 
-if [ -z $respuesta3 ]; then
+if [ -z "$respuesta3" ]; then
     echo "Respuesta erronea. Se aborta el programa"
     exit 0
 fi
