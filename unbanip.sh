7ed386adcdf067c6c949ccc21f43801cc8b611d8
neoranger <neoranger@disroot.org>
2019-01-16 15:25:23 -0300

    modified:   unbanip.sh

--- unbanip.sh
+++ unbanip.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 echo "Conoce la IP a desbloquear? Y/N"
-read respuesta1
+read respuesta1 
+if [ -z $respuesta1 ]; then
+    echo "Respuesta erronea o vacía. Se aborta el programa"
+    exit 0
+fi
 
 if [ "$respuesta1" == "Y" ] || [ "$respuesta1" == "y" ]; then
-	echo "Ingresar IP a desbanear: "
-	read ip
+    echo "Ingresar IP a desbanear: "
+    read ip
+    if [ -z $respuesta1 ]; then
+        echo "Respuesta erronea o vacía. Se aborta el programa"
+    exit 0
+fi
+
 else
-	sudo iptables -L -n
-	echo "----------------------------------------------------------"
-	echo "Ingresar IP a desbanear: "
-        read ip
+     sudo iptables -L -n
+     echo "----------------------------------------------------------"
+     echo "Ingresar IP a desbanear: "
+     read ip
 fi
 
 echo "----------------------------------------------------------"
@@ -18,14 +27,19 @@ echo "----------------------------------------------------------"
 echo "Conoce la celda? Y/N"
 read respuesta2
 
+if [ -z $respuesta2 ]; then
+    echo "Respuesta erronea o vacía. Se aborta el programa"
+    exit 0
+fi
+
 if [ "$respuesta2" == "Y" ] || [ "$respuesta2" == "y" ]; then
-        echo "Ingresar nombre de celda: "
-        read jail
+    echo "Ingresar nombre de celda: "
+    read jail
 else
-        sudo fail2ban-client status
-        echo "----------------------------------------------------------"
-        echo "Ingresar nombre de celda: "
-        read jail
+    sudo fail2ban-client status
+    echo "----------------------------------------------------------"
+    echo "Ingresar nombre de celda: "
+    read jail
 fi
 
 echo "----------------------------------------------------------"
@@ -33,9 +47,14 @@ echo "----------------------------------------------------------"
 echo "La jail a usar es: $jail y la ip a desbanear es: $ip. Es correcto? Y/N"
 read respuesta3
 
+if [ -z $respuesta3 ]; then
+    echo "Respuesta erronea. Se aborta el programa"
+    exit 0
+fi
+
 if [ "$respuesta3" == "Y" ] || [ "$respuesta3" == "y" ]; then
-	sudo fail2ban-client set "$jail" unbanip "$ip"
+    sudo fail2ban-client set "$jail" unbanip "$ip"
 else
-	echo "Se aborta el programa"
-	exit 0
+    echo "Se cancela el programa"
+    exit 0
 fi
